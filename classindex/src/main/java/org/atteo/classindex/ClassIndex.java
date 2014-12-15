73e0fc799807ec0642b6cd60d4bbe101daa9600e
Angelo Suzuki <angelo.suzuki@getbux.com>
2014-12-15 12:17:36 +0100

    Using the InputStreamReader string charset constructor for JB compatibility

--- classindex/src/main/java/org/atteo/classindex/ClassIndex.java
+++ classindex/src/main/java/org/atteo/classindex/ClassIndex.java
@@ -19,7 +19,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.annotation.Annotation;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.HashSet;
@@ -348,7 +347,7 @@ public class ClassIndex {
 			return null;
 		}
 		try {
-			try (BufferedReader reader = new BufferedReader(new InputStreamReader(resource.openStream(), StandardCharsets.UTF_8))) {
+			try (BufferedReader reader = new BufferedReader(new InputStreamReader(resource.openStream(), "UTF-8"))) {
 				StringBuilder builder = new StringBuilder();
 				String line = reader.readLine();
 				while (line != null) {
@@ -379,7 +378,7 @@ public class ClassIndex {
 
 			while (resources.hasMoreElements()) {
 				URL resource = resources.nextElement();
-				try (BufferedReader reader = new BufferedReader(new InputStreamReader(resource.openStream(), StandardCharsets.UTF_8))) {
+				try (BufferedReader reader = new BufferedReader(new InputStreamReader(resource.openStream(), "UTF-8"))) {
 
 					String line = reader.readLine();
 					while (line != null) {
